1218133717edf4a71fc064b6c7609b60a4a6380e
bevus <hacene.kedjar@gmail.com>
2017-01-26 18:42:32 +0100

    created empty controllers

---
+++ bankApp/bankApp/Controllers/BankerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bankApp.Controllers
+{
+    public class BankerController : Controller
+    {
+        // GET: Banker
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
